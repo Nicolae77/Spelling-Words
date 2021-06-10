ab5121cf0b4315235be3e9da1212ddaa3689d0aa
Nicolae <67197181+Nicolae77@users.noreply.github.com>
2021-06-10 12:48:03 +0100

    Update main.py

--- Spelling Alphabet/main.py
+++ Spelling Alphabet/main.py
@@ -2,7 +2,7 @@ import pandas
 
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
-# print(phonetic_dict)
+print(phonetic_dict)
 word = input("Enter a word: ").upper()
 result = [phonetic_dict[letter] for letter in word]
 print(result)
