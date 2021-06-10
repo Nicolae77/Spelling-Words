ea36e930aa5b523cec3b32f9614b01f3a965414e
Nicolae <67197181+Nicolae77@users.noreply.github.com>
2021-06-10 12:51:03 +0100

    Update main.py

--- Spelling Alphabet/main.py
+++ Spelling Alphabet/main.py
@@ -3,6 +3,11 @@ import pandas
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 print(phonetic_dict)
-word = input("Enter a word: ").upper()
-result = [phonetic_dict[letter] for letter in word]
-print(result)
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
+    try:
+        result = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry only letters in the alphabet please.")
+    else:
+    print(result)
